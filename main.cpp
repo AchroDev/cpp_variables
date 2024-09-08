7f97fd8ec80514c594295bd08699bee137329a74
AchroDev <achrodev@proton.me>
2024-09-07 22:14:11 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int x = 420;
+    int y = 69;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+
+    return 0;
+}
